154b0a200cca9bfe2cad2632038a3ec64fef0404
Gidua Server <flavyjala@hotmail.com>
2024-12-23 16:48:07 +0000

    fix: almost made it work

--- index.js
+++ index.js
@@ -21,7 +21,35 @@ app.post('/wb', (req, res) => {
 		console.log('Commit:', commitMessage);
 	}
 
-	exec('sh script.mjs', (error, stdout, stderr) => {
+	exec('git config --global --add safe.directory /var/www/example.org/tudeli', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return res.status(500).send('Error executing script');
+		}
+		console.log(`stdout: ${stdout}`);
+		console.error(`stderr: ${stderr}`);
+	});
+
+	exec('git -C /var/www/example.org/tudeli reset --hard', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return res.status(500).send('Error executing script');
+		}
+		console.log(`stdout: ${stdout}`);
+		console.error(`stderr: ${stderr}`);
+	});
+
+	console.log('ABOUT TO PULL')
+
+	exec('git -C /var/www/example.org/tudeli pull', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return res.status(500).send('Error executing script');
+		}
+		console.log(`stdout: ${stdout}`);
+		console.error(`stderr: ${stderr}`);
+	});
+	exec('npm run --prefix /var/www/example.org/tudeli build', (error, stdout, stderr) => {
 		if (error) {
 			console.error(`exec error: ${error}`);
 			return res.status(500).send('Error executing script');
