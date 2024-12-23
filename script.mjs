ca74e90cb7fb7193487ef163a9124b076693e5ee
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 02:48:34 -0300

    feat: changed script front

--- script.mjs
+++ script.mjs
@@ -2,8 +2,6 @@
 import 'zx/globals'
 
 
-const move_front = await $`cd /var/www/example.org/tudeli`
-const pull_front = await $`git pull`
-const build_front = await $`bash -c "npm run build"`
+const move_front = await $`cd /var/www/example.org/tudeli && git pull && npm run build`
 
 
