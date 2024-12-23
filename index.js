85aaab5795398f8b001d0c682193ae74e6bc7448
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 01:45:49 -0300

    feat: first test

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const app = express();
+
+const port = 5555;
+
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+
+	console.log('Received webhook request:', req.body);
+
+	res.status(200).send('Webhook received!');
+
+});
+
+app.listen(port, () => {
+
+	console.log(`Server is running on port ${port}`);
+
+});
