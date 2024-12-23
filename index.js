044ebad119e1f618e3350aad52f8cef481332b7f
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 13:12:19 -0300

    Revert "test: using zx directly"
    This reverts commit 6305675634323fa62b77d23c81a0ca9f1843daeb.

--- index.js
+++ index.js
@@ -1,34 +1,37 @@
 const express = require('express');
 const { exec } = require('child_process');
 const bodyParser = require('body-parser');
-const zx = require('zx');
 
 const app = express();
 const port = 5555;
 
 app.use(bodyParser.json());
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/wb', async (req, res) => {
+app.post('/wb', (req, res) => {
 	console.log('Received webhook request:', req.body);
 	console.log('req: ', req);
+
 
 	const commitMessage = req.body.head_commit ? req.body.head_commit.message : null;
 
 	if (commitMessage) {
 		console.log('Commit:', commitMessage);
-
-		try {
-			await $`node /var/www/example.org/auto_build/script.mjs`;
-			res.status(200).send('Webhook received and script executed!');
-		} catch (error) {
-			console.error(`Error executing script: ${error}`);
-			res.status(500).send('Error executing script');
-		}
 	}
+
+	exec('node /var/www/example.org/auto_build/script.mjs', (error, stdout, stderr) => {
+		if (error) {
+			console.error(`exec error: ${error}`);
+			return res.status(500).send('Error executing script');
+		}
+		console.log(`stdout: ${stdout}`);
+		console.error(`stderr: ${stderr}`);
+		res.status(200).send('Webhook received and script executed!');
+	});
 });
 
 app.listen(port, () => {
 	console.log(`Server is running on port ${port}`);
 });
-
