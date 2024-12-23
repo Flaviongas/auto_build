88a5dddbfd806e73ab2e34742ad36071bd41ff39
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 02:41:05 -0300

    fix: typo on command

--- script.mjs
+++ script.mjs
@@ -4,6 +4,6 @@ import 'zx/globals'
 
 const move_front = await $`cd /var/www/example.org/tudeli`
 const pull_front = await $`git pull`
-const build_front = await $`bash .c "npm run build"`
+const build_front = await $`bash -c "npm run build"`
 
 
