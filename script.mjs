a0221db37de7b5a3457dcf71299eec5d3366a182
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 13:19:59 -0300

    test: imported $

--- script.mjs
+++ script.mjs
@@ -1,5 +1,8 @@
 #!/usr/bin/env zx
 import 'zx/globals'
+import { $ } from 'zx';
+
+await $`echo "Hello, world!"`;
 
 
 const move_front = await $`cd /var/www/example.org/tudeli && git reset --hard && git pull && npm run build`
