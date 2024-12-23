a281169123c37332e25dee56fb99f12092eda66a
flavio02 (NixOS) <flavyjala@hotmail.com>
2024-12-23 13:24:39 -0300

    test: maybe using sh instead of node

--- script.mjs
+++ script.mjs
@@ -1,8 +1,4 @@
 #!/usr/bin/env zx
-import 'zx/globals'
-import { $ } from 'zx';
-
-await $`echo "Hello, world!"`;
 
 
 const move_front = await $`cd /var/www/example.org/tudeli && git reset --hard && git pull && npm run build`
