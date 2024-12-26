750283a252d02bab83e325dd04c369d8c50c3233
Gidua Server <flavyjala@hotmail.com>
2024-12-26 03:28:23 +0000

    feat: updated bash path

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ app.post('/wb', (req, res) => {
 
 	setTimeout(() => {
 
-		exec('npm run build', { cwd: '/var/www/example.org/tudeli', shell: '/run/current-system/sw/bin/bash' }, (error, stdout, stderr) => {
+		exec('npm run build', { cwd: '/var/www/example.org/tudeli', shell: '/nix/store/4006pcsz94vkxsfv6hqmb36dqd6j6r8x-system-path/bin/bash' }, (error, stdout, stderr) => {
 			if (error) {
 				console.error(`exec error: ${error}`);
 				return res.status(500).send('Error executing script');
